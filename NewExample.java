c3296319a11e3fe59db873f710f3c716d8733a9d
matthewmoulds <matthew.moulds1@gmail.com>
2015-08-16 11:33:35 +0100

    Switching back to master branch

---
+++ NewExample.java
@@ -0,0 +1,9 @@
+/**
+ * Created by matthewmoulds on 16/08/15.
+ */
+public class NewExample {
+    public NewExample() {
+        System.out.println("Hello World!");
+        System.out.println("I'm trying to switch back to the master branch");
+    }
+}
