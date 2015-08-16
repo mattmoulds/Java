cac6e5d1b2cc867d97589c666108dd393fbcc366
matthewmoulds <matthew.moulds1@gmail.com>
2015-08-16 15:17:59 +0100

    ForDemo added

---
+++ ForDemo.java
@@ -0,0 +1,19 @@
+/**
+ * Created by matthewmoulds on 16/08/15.
+ */
+/*
+* Demonstrate the for loop
+*
+* Call this file ForDemo.java.
+*/
+class ForDemo {
+    public static void main(String args[]) {
+        int count;
+
+        //for(initialization; condition; iteration) statement;
+        for(count = 0; count < 5; count = count++)
+            System.out.println("This is count: " + count);
+
+        System.out.println("Done!");
+    }
+}
