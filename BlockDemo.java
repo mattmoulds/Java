739fbea143d8f1a7890d4fa3ec5d909507ea2d6f
matthewmoulds <matthew.moulds1@gmail.com>
2015-08-16 15:48:18 +0100

    BlockDemo added

---
+++ BlockDemo.java
@@ -0,0 +1,22 @@
+/**
+ * Created by matthewmoulds on 16/08/15.
+ */
+/*
+* Demonstrate a block of code
+* Call this file BlockDemo.java
+*/
+class BlockDemo {
+    public static void main(String args[]) {
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        // the target of this if is a block
+        if(i != 0){
+            System.out.println("i does not equal 0");
+            d = j / i;
+            System.out.println("j / i id " + d);
+        }
+    }
+}
