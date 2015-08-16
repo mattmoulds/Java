95538053dccc0ee0ff58aa408fcc66a5602f646e
matthewmoulds <matthew.moulds1@gmail.com>
2015-08-16 18:31:35 +0100

    Created InchToMeters to convert inches to meters for 12 feet.

---
+++ InchToMeters.java
@@ -0,0 +1,28 @@
+/**
+ * Created by matthewmoulds on 16/08/15.
+ */
+/*
+* Self Test
+* Convert inches to meters
+*/
+
+class InchToMeters {
+    public static void main(String args[]) {
+        double inch, meter, feet;
+        int counter;
+
+        feet =  12 * 12;
+
+        counter = 0;
+        for(inch = 1; inch <= feet; inch++) {
+            meter = inch * 39.37;
+            System.out.println(inch + " inches equals " + meter + "meters");
+
+            counter++;
+            if(counter == 12) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
