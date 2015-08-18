55b4f4470cce56432d52a653237b643f44cefb3c
matthewmoulds <matthew.moulds1@gmail.com>
2015-08-18 22:45:19 +0100

    2 - 3 Character Arithmetic

---
+++ CharArithDemo.java
@@ -0,0 +1,23 @@
+/**
+ * Created by matthewmoulds on 18/08/15.
+ */
+/*
+* Character variables can be handled like integers
+*
+* 2 - 3
+*/
+
+class CharArithDemo {
+    public static void main(String args[]) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++; // increment ch
+        System.out.println("ch is now " + ch);
+
+        ch = 90; // give ch the value of z
+        System.out.println("ch is now " + ch);
+    }
+}
