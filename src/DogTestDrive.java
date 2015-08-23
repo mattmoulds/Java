978157c3780f82282a7cc3ecd8a30918040e071f
matthew.moulds1@gmail.com <leicester1884>
2015-08-23 18:07:26 +0100

    HFL Making my first object!

---
+++ src/DogTestDrive.java
@@ -0,0 +1,11 @@
+/**
+ * Created by matt on 23/08/15.
+ */
+class DogTestDrive {
+    public static void main(String args[]){
+        // Dog test code goes here
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
