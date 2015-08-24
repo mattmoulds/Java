98291895f029c3590460a7dba80789f04f1387fa
Matt <matthew.moulds1@gmail.com>
2015-08-24 21:02:42 +0100

    Encapsulation example using Dogs as an example.

---
+++ src/HeadFirstJava/DogEncapsulation/GoodDog.java
@@ -0,0 +1,26 @@
+package HeadFirstJava.DogEncapsulation;
+
+/**
+ * Created by matt on 24/08/15.
+ */
+public class GoodDog {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Woof! Woof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
---
+++ src/HeadFirstJava/ThirdDog/Dog.java
@@ -0,0 +1,19 @@
+package HeadFirstJava.ThirdDog;
+
+/**
+ * Created by matt on 24/08/15.
+ */
+public class Dog {
+    int size;
+    String name;
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Woof! Woof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
