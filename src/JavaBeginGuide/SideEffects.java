0cb2f69f146ef1801fee1f192474d2e9b88e653f
Matt <matthew.moulds1@gmail.com>
2015-09-12 12:42:56 +0100

    Side effect of using s short circuit operator

---
+++ src/JavaBeginGuide/SideEffects.java
@@ -0,0 +1,29 @@
+package JavaBeginGuide;
+
+/**
+ * Created by matt on 12/09/15.
+ */
+public class SideEffects {
+    // side effects can be important.
+    public static void main(String args[]) {
+        int i;
+
+        i = 0;
+
+        /*
+        Here, i is still incremented even though
+        the if statement fails.
+        */
+        if(false & (++i < 100))
+            System.out.println("This won't be displayed");
+        System.out.println("if statement executed: " + i); // displays 1
+
+        /*
+        In this case, i is not incremented because
+        the short-circuit operator skips the increment.
+        */
+        if(false && (++i < 100))
+            System.out.println("this won't be displayed");
+        System.out.println("if statement executed: " + i); // still
+    }
+}
