ae4b2e8159bbb012e469d6740ba86c6e53133e59
Matt <matthew.moulds1@gmail.com>
2015-09-20 18:15:32 +0100

    Added a polymorphism example but left the contents commented out.

---
+++ src/HeadFirstJava/PolymorphicAnimal/PolyAnimal.java
@@ -0,0 +1,22 @@
+package HeadFirstJava.PolymorphicAnimal;
+
+/**
+ * Created by matt on 20/09/15.
+
+public class PolyAnimal {
+    public static void main(String args[]) {
+        Animal[] animals = new Animal[5];
+        animals [0] = new Dog();
+        animals [1] = new Cat();
+        animals [2] = new Wolf();
+        animals [3] = new Hippo();
+        animals [4] = new Lion();
+
+        for (int i = 0; i < animals.length; i++) {
+            animals[i].eat();
+
+            animals[i].roam();
+        }
+    }
+}
+ */
