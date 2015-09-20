7134c33660b82dc764ca2e4dd9b63351eb59bddd
Matt <matthew.moulds1@gmail.com>
2015-09-20 23:00:06 +0100

    Overloading a method example.

---
+++ src/HeadFirstJava/Overloads.java
@@ -0,0 +1,28 @@
+package HeadFirstJava;
+
+/**
+ * Created by matt on 20/09/15.
+ */
+public class Overloads {
+
+    String uniqueID;
+
+    public int addNums(int a, int b) {
+        return a + b;
+    }
+
+    public double addNums(double a, double b) {
+        return a + b;
+    }
+
+    public void setUniqueID(String theID) {
+        uniqueID = theID;
+    }
+
+    public void setUniqueID(int ssNumber) {
+        String numString = "" + ssNumber;
+        setUniqueID(numString);
+    }
+
+}
+
